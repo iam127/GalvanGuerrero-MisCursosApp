3a37af2a37f49c50958dd7e6e300deb9e08263dc
Matias Galvan Guerrero <matias.galvan@tecsup.edu.pe>
2025-11-23 17:57:37 -0500

    Configurar MainActivity

--- app/src/main/java/com/tecsup/galvanguerreromiscursosapp/MainActivity.kt
+++ app/src/main/java/com/tecsup/galvanguerreromiscursosapp/MainActivity.kt
@@ -4,44 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.FirebaseApp
+import com.tecsup.galvanguerreromiscursosapp.navigation.AuthApp
 import com.tecsup.galvanguerreromiscursosapp.ui.theme.GalvanGuerreroMisCursosAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // Inicializar Firebase
+        FirebaseApp.initializeApp(this)
+
         setContent {
             GalvanGuerreroMisCursosAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AuthApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GalvanGuerreroMisCursosAppTheme {
-        Greeting("Android")
     }
 }
